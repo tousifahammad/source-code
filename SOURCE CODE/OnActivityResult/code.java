35136ae7797dd1778e4e34fc2c2828e9498b7c13
Tousif Ahammad <tousif@kazmatechnology.com>
2019-11-21 12:28:55 +0530

    21-11-19 12 28 pm

--- SOURCE CODE/OnActivityResult/code.java
+++ SOURCE CODE/OnActivityResult/code.java
@@ -22,9 +22,51 @@ startActivityForResult(intent , REQUEST_CODE);
         }
 
     }
-In custom dialog activity use this code to set result
+
+
+//In custom dialog activity use this code to set result
+
 
 Intent intent = getIntent();
 intent.putExtra("key", value);
 setResult(RESULT_OK, intent);
 finish();
+
+
+
+
+============================================================================
+From your FirstActivity call the SecondActivity using startActivityForResult() method
+
+For example:
+
+Intent i = new Intent(this, SecondActivity.class);
+startActivityForResult(i, 1);
+In your SecondActivity set the data which you want to return back to FirstActivity. If you don't want to return back, don't set any.
+
+For example: In SecondActivity if you want to send back data:
+
+Intent returnIntent = new Intent();
+returnIntent.putExtra("result",result);
+setResult(Activity.RESULT_OK,returnIntent);
+finish();
+If you don't want to return data:
+
+Intent returnIntent = new Intent();
+setResult(Activity.RESULT_CANCELED, returnIntent);
+finish();
+Now in your FirstActivity class write following code for the onActivityResult() method.
+
+@Override
+protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+    if (requestCode == 1) {
+        if(resultCode == Activity.RESULT_OK){
+            String result=data.getStringExtra("result");
+        }
+        if (resultCode == Activity.RESULT_CANCELED) {
+            //Write your code if there's no result
+        }
+    }
+}//onActivityResult
+
